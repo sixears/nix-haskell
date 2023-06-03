7689b75579815625420372ed8b2ff677e94a9a23
Mr. Fluffy <git@sixearsm>
2023-06-03 14:04:54 +0000

    r0.0.1.0

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+0.0.1.0 2023-06-03
+==================
+- use hpkgs 0.0.15.0
+
 0.0.0.0 2023-05-29
 ===================
 - initial commit; moved from ~/rc/nixpkgs/haskell
--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url     = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     flake-utils.url = github:numtide/flake-utils/c0e246b9;
     # hpkgs1.url = path:/home/martyn/src/hpkgs1;
-    hpkgs1.url      = github:sixears/hpkgs1/r0.0.14.0;
+    hpkgs1.url      = github:sixears/hpkgs1/r0.0.15.0;
   };
 
   outputs = { self, nixpkgs, flake-utils, hpkgs1 }:
